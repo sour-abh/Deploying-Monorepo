44989631c71c149b132c33b8dfc40da5d53bd1ca
Sourabh kumar <54701904+sour-abh@users.noreply.github.com>
2025-12-17 07:53:31 +0530

    initial commit

---
+++ packages/db/index.ts
@@ -0,0 +1,21 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+import { Pool } from "pg";
+const pool=new Pool({
+    connectionString:process.env.DATABASE_URL
+})
+
+const adapter=new PrismaPg(pool)
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({adapter})
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
